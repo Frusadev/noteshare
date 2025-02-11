a1ecf9e63ac9d2d7dfb1d0fae6af22557cb724f8
Frusadev <frusadev@gmail.com>
2025-02-11 00:55:37 +0000

    Fix body param

--- src/app/api/notes/add/route.ts
+++ src/app/api/notes/add/route.ts
@@ -2,19 +2,16 @@ import { NextResponse } from "next/server";
 import type { INote } from "../../db/schemas/Note";
 import { addNote } from "../../db/queries";
 
-export async function POST(
-  req: Request,
-  { body }: { body: Promise<{ note: INote }> },
-) {
-  const bod = await req.json();
-  console.log(bod.date)
-  console.log(bod)
+export async function POST(req: Request) {
+  const body = await req.json();
+  console.log(body.date);
+  console.log(body);
   const note: INote = {
-    id: bod.note.id,
-    authorName: bod.note.authorName,
-    content: bod.note.content,
-    date: new Date(Date.parse(bod.note.date)),
-  }
-  addNote(note)
-  return NextResponse.json({id: note.id})
+    id: body.note.id,
+    authorName: body.note.authorName,
+    content: body.note.content,
+    date: new Date(Date.parse(body.note.date)),
+  };
+  addNote(note);
+  return NextResponse.json({ id: note.id });
 }
